0251336ed1eb99a60c319153d7de0497edb81695
Replit user <>
2023-06-26 14:31:07 +0200

    Remove ignored files

--- datacenter_time_format.py
+++
@@ -1,5 +0,0 @@
-def format_duration(duration):
-    hours = duration.seconds // 3600
-    minutes = (duration.seconds % 3600) // 60
-    formatted_duration = f'{hours:02}:{minutes:02}'
-    return formatted_duration
